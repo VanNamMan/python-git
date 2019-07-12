20bfcb749f44f2987c809f5f707f3d8f19234282
VanNamMan <manvannamk54@gmail.com>
2019-07-12 23:14:56 +0700

    cursor in tkinter

--- opencv_tkinter/libs/myCanvas.py
+++ opencv_tkinter/libs/myCanvas.py
@@ -18,17 +18,23 @@ class myCanvas(tk.Canvas):
         self.bDrawLine = False
         self.bActiveRect = False
         self.start = None
-        self.image = None
+
+        self.photoImg = None
+        self.crop = None
+
         self.rect = [0,0,0,0]
         self.rect_active = [0,0,0,0]
         self.id_rect_active = -1
         self.rects = []
         self.color = "red"
+        self.faceColor = "yellow"
+        self.font = "Times 14 bold"
         self.lw = 3
 
         self.bind("<ButtonPress-1>",self.lButtonDown)
         self.bind("<Motion>",self.mouseMove)
         self.bind("<ButtonRelease-1>",self.lButtonUp)
+
     def lButtonDown(self,e):
         p = [e.x,e.y]
         for r in self.rects:
@@ -39,7 +45,7 @@ class myCanvas(tk.Canvas):
                 self.rect_active = r
                 self.create_rectangle(r[0],r[1],r[2],r[3]
                                     ,outline="yellow",width=self.lw)
-                
+ 
                 break
         if not self.bDrawRect:
             return
@@ -54,38 +60,62 @@ class myCanvas(tk.Canvas):
         self.bActiveRect = False
         
     def mouseMove(self,e):
+        p = [e.x,e.y]
+        bCursor = False
+        idRect = False
+
+        bCursor,idRect = self.checkPoint(p)
+        if bCursor:
+            self.configure(cursor="hand2")
+            self.reDrawCanvas(self.rects,idRect)
+        else:
+            self.configure(cursor="plus")
+
         if not self.bDrawRect:
             return
-        self.delete("all")
-        if self.bDrawAlign:
-            self.create_line(0,e.y,self.winfo_width(),e.y
-                                ,fill=self.color,width=self.lw)
-            self.create_line(e.x,0,e.x,self.winfo_height()
-                                ,fill=self.color,width=self.lw)
         if self.bActiveRect:
             dx = e.x - self.start.x
             dy= e.y - self.start.y
             r = self.rect_active
             self.rects[self.id_rect_active] = [r[0]+dx,r[1]+dy,r[2]+dx,r[3]+dy]
-            self.create_rectangle(r[0]+dx,r[1]+dy,r[2]+dx,r[3]+dy
-                                    ,outline="yellow",width=self.lw)
-            for i in range(len(self.rects)):
-                if i != self.id_rect_active:
-                    r = self.rects[i]
-                    self.create_rectangle(r[0],r[1],r[2],r[3]
-                                    ,outline=self.color,width=self.lw)
+            self.reDrawCanvas(self.rects,self.id_rect_active)
+            self.crop = self.croped(self.rects[self.id_rect_active])
             pass
-        elif not self.bDrawAlign:
-            if self.image:
-                self.showImage(self.image)
+        else :
+            if self.bDrawAlign:
+                self.reDrawCanvas(self.rects,-1)
+                self.create_line(0,e.y,self.winfo_width(),e.y
+                                    ,fill=self.color,width=self.lw)
+                self.create_line(e.x,0,e.x,self.winfo_height()
+                                    ,fill=self.color,width=self.lw)
+            else:
+                self.reDrawCanvas(self.rects,-1)
+                if self.start:
+                    self.rect = [self.start.x,self.start.y,e.x,e.y]
+                    self.create_rectangle(self.start.x,self.start.y,e.x,e.y
+                                    ,outline=self.color,width=self.lw)
+    def reDrawCanvas(self,rects,idActive):
+        self.delete("all")
+        if self.photoImg:
+                self.showImage(self.photoImg)
+        for i in range(len(self.rects)):
+            r = self.rects[i]
+            if i != idActive:
+                color = self.color
+            else:
+                color = "yellow"
+            self.create_rectangle(r[0],r[1],r[2],r[3]
+                                    ,outline=color,width=self.lw)
+            self.create_text(r[0]-5,r[1]-5,fill=self.faceColor,font=self.font,
+                    text="%d"%i)
 
-            for r in self.rects:
+    def checkPoint(self,p):
+        for r in self.rects:
+            if self.ptInRect(p,r):
                 self.create_rectangle(r[0],r[1],r[2],r[3]
-                                ,outline=self.color,width=self.lw)
-            if self.start:
-                self.rect = [self.start.x,self.start.y,e.x,e.y]
-                self.create_rectangle(self.start.x,self.start.y,e.x,e.y
-                                ,outline=self.color,width=self.lw)
+                                    ,outline="yellow",width=self.lw)
+                return True,self.rects.index(r)
+        return False,-1
     def ptInRect(self,p,r):
         if r[0] < p[0] < r[2] and r[1] < p[1] < r[3]:
             return True
@@ -93,14 +123,18 @@ class myCanvas(tk.Canvas):
             return False
         pass
     def resetCanvas(self):
-        self.delete("all")
         self.rects = []
-    def toCvRect(self):
-        print(self.image.width())
-        pass
-    def showImage(self,img):
-        self.image = img
-        self.create_image(1,1,anchor="nw",image=img)
+        self.reDrawCanvas(self.rects,-1)
+    def croped(self,r):
+        x1,y1,x2,y2 = r
+        wRoot = self.winfo_width()
+        hRoot = self.winfo_height()
+        x1,y1,x2,y2= [max(x1/wRoot,0),max(y1/hRoot,0),min(x2/wRoot,1),min(y2/hRoot,1)]
+        return [x1,y1,x2,y2]
+    def showImage(self,photoImg):
+        self.photoImg = photoImg
+        self.create_image(1,1,anchor="nw",image=photoImg)
+
             
 
 
--- opencv_tkinter/myGui.py
+++ opencv_tkinter/myGui.py
@@ -17,10 +17,12 @@ class myGui(tk.Frame):
 	def __init__(self):
 		super().__init__()
 		self.file_types = [".jpg",".bmp",".png",".gif"]
-		self.pilImg = None
+		# self.pilImg = None
+		self.image = None
 		self.initUI()
 	def initUI(self):	
 		self.master.title("Gui Tkinter")
+		self.master.bind("<Motion>",self.mouseMove)
 		# menubar File , Camera , Setting , Font
 		menubar = Menu(self.master)
 		self.master.config(menu=menubar)
@@ -59,10 +61,10 @@ class myGui(tk.Frame):
 
 		# f1 Frame 
 		# ======== toolbar =========
-		toolbar = Frame(f1,height=10,relief=RAISED,bg=GREEN2)
+		toolbar = Frame(f1,height=3,relief=RAISED,bg=LIGHT_BLUE1)
 		picontrol = Frame(f1,height=590)
 		toolbar.pack(fill="x",side="top")
-		picontrol.pack(fill="both",expand=1,side="top")
+		picontrol.pack(fill="both",expand=1,side="top",anchor=W)
 
 		self.icon1 = ImageTk.PhotoImage(Image.open("res/rect.png"))
 		Button(toolbar,image=self.icon1,relief=FLAT
@@ -115,6 +117,8 @@ class myGui(tk.Frame):
 		Button(f22,text="Reset",font=font,image=self.icon5,compound=LEFT
 			,command=lambda:self.command(RESET)).pack(side="left",fill="both",expand=1,anchor="s",padx=2,pady=2)
 
+		self.miniCanvas = myCanvas(f21,bg=BLACK)
+		self.miniCanvas.pack(fill="both",expand=1,anchor="s")
 		self.logText = tk.Listbox(f21)
 		self.logText.pack(fill="both",expand=1,anchor="s")
 
@@ -130,6 +134,7 @@ class myGui(tk.Frame):
 			self.load()
 		elif cmd == RECT:
 			self.canvas.bDrawRect = True
+			self.canvas.bDrawAlign = True
 		elif cmd == ADD:		
 			self.canvas.rects.append(self.canvas.rect)
 		elif cmd == CLEAR_ALL:
@@ -140,6 +145,20 @@ class myGui(tk.Frame):
 		print ("pressed",e.char)   
 		if e.char == "a":
 			self.command(RECT)
+	def mouseMove(self,e):
+		if self.image is None:
+			return
+		r = self.getGeometry(self.canvas)
+		p = [e.x,e.y]
+		if not self.canvas.ptInRect(p,r):
+			return
+		if self.canvas.crop:
+			x1,y1,x2,y2 = self.canvas.crop
+			h,w = self.image.shape[:2]
+			x1,y1,x2,y2 = int(x1*w),int(y1*h),int(x2*w),int(y2*h)
+			print(x1,y1,x2,y2)
+			crop = self.image[y1:y2,x1:x2]
+			self.show(self.miniCanvas,crop)
     # function
 	def log(self,txt):
 		txt = time.strftime("%H:%M:%S ")+txt
@@ -155,18 +174,22 @@ class myGui(tk.Frame):
 			_, file_extension = os.path.splitext(filename)
 			if file_extension in self.file_types:
 				self.label["text"] = filename
-				img = cv2.imread(filename)
-				self.show(self.canvas,img)
+				self.image = cv2.imread(filename)
+				self.show(self.canvas,self.image)
+	def getGeometry(self,item):
+		r = [item.winfo_x(),item.winfo_y(),item.winfo_width(),item.winfo_height()]
+		return r
+
 	def show(self,canvas,img):
-	    global pilImg
 	    size = (canvas.winfo_width(),canvas.winfo_height())
 	    if len(img.shape) == 2:
-	    	pilImg = cv2.resize(img,size)
+	    	cvImg = cv2.resize(img,size)
 	    else:
-	    	pilImg = cv2.resize(img[...,::-1],size)  # BGR to RGB 
+	    	cvImg = cv2.resize(img[...,::-1],size)  # BGR to RGB 
 
-	    pilImg = ImageTk.PhotoImage(image=Image.fromarray(pilImg))
-	    canvas.showImage(pilImg)
+	    pilImg = Image.fromarray(cvImg)
+	    photoImg = ImageTk.PhotoImage(image=pilImg)
+	    canvas.showImage(photoImg)
 
 
 
